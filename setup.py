3e92404e531f86e5d49a442b667724efe3d01241
Aditya Verma <coderadi@Adityas-MacBook-Pro.local>
2020-06-02 03:25:22 +0530

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='pycollections',
+    version='0.0.1',
+    maintainer='Aditya Verma',
+    maintainer_email='aditya.verma.connect@gmail.com',
+    packages=['pycollections'],
+    install_requires=[]
+)
